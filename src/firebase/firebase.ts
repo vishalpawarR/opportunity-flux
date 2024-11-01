315ab24a1fb402f185e1ab5a9698916aea6152da
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-11-01 09:03:18 +0530

    moved to the new firebase

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,26 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, FirebaseApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
 import { getAuth, Auth } from "firebase/auth"
 import { getFirestore, Firestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcAmpfVuonsvBu2h9PNgdCefO3TzJifSw",
-  authDomain: "opportunity-flux.firebaseapp.com",
-  projectId: "opportunity-flux",
-  storageBucket: "opportunity-flux.appspot.com",
-  messagingSenderId: "422561337060",
-  appId: "1:422561337060:web:0eccb126880a92f63c66f1",
-  measurementId: "G-KY4S3ZJZRV",
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 }
 
-// Initialize Firebase
 export const app: FirebaseApp = initializeApp(firebaseConfig)
 export const analytics = getAnalytics(app)
 export const auth: Auth = getAuth()
